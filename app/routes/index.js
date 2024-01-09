20e62368cb7a2c31ed6519152ce915c359c90a0c
vikiru <72267229+vikiru@users.noreply.github.com>
2024-01-08 23:09:08 -0500

    Add planned routes

---
+++ app/routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+router.use()
+module.exports = router;
+
+// router.get('/dinosaurs/)
+
+// router.get('/dinsaours/:id)
+
+// router.get('/dinosaurs/:name')
+
+// router.get('/dinosaurs/random/:count')
+
+// router.get() - query params
