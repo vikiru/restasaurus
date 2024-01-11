c7e40d3e900eb2a949ccb796050249f2052dacbe
vikiru <72267229+vikiru@users.noreply.github.com>
2024-01-11 10:51:18 -0500

    Add routes to main app file and update middlewares

--- app/app.js
+++ app/app.js
@@ -3,19 +3,23 @@ const app = express();
 const config = require("./config/index");
 const mongoDB = require("./data/mongoData");
 const middlewares = require("./middlewares/index");
+const routes = require("./routes/index.js");
 
 /* Connect to MongoDB database, log success and error messages (if any)*/
 mongoDB.connect();
 
 /* Setup middlewares */
 app.use(middlewares.bodyParser.json());
-app.use(middlewares.bodyParser.urlencoded());
+app.use(middlewares.bodyParser.urlencoded({ extended: true }));
 app.use(middlewares.cors());
 app.use(middlewares.compression());
 app.use(middlewares.morgan("dev"));
 app.use(middlewares.helmet());
-app.use(middlewares.limiter);
 
 app.listen(config.port, async () =>
-	console.log(`restasaurus started on port: http://localhost:${config.port}`),
+	console.log(
+		`restasaurus started on port: http://localhost:${config.port}/api/v1`,
+	),
 );
+
+app.use("/api/v1", routes);
