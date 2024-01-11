57df6b9c622e7446f11757f14190779bd55f43cf
vikiru <72267229+vikiru@users.noreply.github.com>
2024-01-11 16:22:19 -0500

    Cleanup post to db

--- app/services/index.js
+++ app/services/index.js
@@ -1,6 +1,15 @@
-async function pushDinosaurToDB(dinosaur) {
+const { Dinosaur } = require("../models/Dinosaur");
+const { DinosaurInfo } = require("../models/DinosaurInfo");
+const { DinosaurImage } = require("../models/DinosaurImage");
+
+async function pushDinosaurToDB(data) {
 	try {
-		await dinosaur.save();
+		const dinosaur = data.dinosaur;
+		const dinoInfo = data.dinoInfo;
+		const dinoImage = data.dinoImage;
+		await Dinosaur.create(dinosaur);
+		await DinosaurInfo.create(dinoInfo);
+		await DinosaurImage.create(dinoImage);
 		console.log("Successfully saved dinosaur to database");
 	} catch (error) {
 		console.error(error);
