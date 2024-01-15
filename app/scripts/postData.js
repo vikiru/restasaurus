a414f9940e8e509d9f1e1d8701fe58f767e9e5d2
vikiru <72267229+vikiru@users.noreply.github.com>
2024-01-15 15:15:59 -0500

    Update logging for scripts

--- app/scripts/postData.js
+++ app/scripts/postData.js
@@ -6,11 +6,22 @@ const mongoDB = require("../data/mongoData");
 
 async function postAllDinosaurs() {
 	await mongoDB.connect();
+	logger.info("Starting to save dinosaur data to MongoDB database.");
+	const startTime = process.hrtime();
+
 	const dinosaurPromises = dinosaurData.map(async dinosaur => {
 		const dinosaurSchema = await convertToSchema(dinosaur);
 		return pushDinosaurToDB(dinosaurSchema);
 	});
 	await Promise.all(dinosaurPromises);
+
+	const endTime = process.hrtime(startTime);
+	const timeInSeconds = endTime[0] + endTime[1] / 1e9;
+	const formattedSeconds = timeInSeconds.toFixed(2);
+	logger.info(
+		`Finishing saving all dinosaur data to MongoDB database. Completed in ${formattedSeconds} seconds.`,
+	);
+
 	await mongoDB.disconnect();
 }
 
--- app/scripts/retrieveData.js
+++ app/scripts/retrieveData.js
@@ -8,20 +8,33 @@ const { logger } = require("../config/logger");
 async function retrieveAllDinoData() {
 	const names = dinosaurNames.names;
 	const data = [];
+	const totalTimeStart = process.hrtime();
 	for (const name of names) {
+		const startTime = process.hrtime();
+
 		let mongooseData = new MongooseData(name);
+		const position = names.indexOf(name) + 1;
+		const total = names.length;
 		logger.info(
-			`Started data retrieval for ${name}, ${names.indexOf(name) + 1} / ${
-				names.length
-			}`,
+			`Started data retrieval for ${name} (${position} of ${total})`,
 		);
 		mongooseData = await retrieveInformation(name, mongooseData);
 		mongooseData = await retrieveHTMLContent(name, mongooseData);
 		data.push(mongooseData);
 
-		logger.info(`Finished data retrieval for ${name}\n`);
+		const endTime = process.hrtime(startTime);
+		const timeInSeconds = endTime[0] + endTime[1] / 1e9;
+		const formattedSeconds = timeInSeconds.toFixed(2);
+		logger.info(
+			`Finished data retrieval for ${name}. Completed in ${formattedSeconds} seconds.\n`,
+		);
 	}
-
+	const totalTimeEnd = process.hrtime(totalTimeStart);
+	const timeInSeconds = totalTimeEnd[0] + totalTimeEnd[1] / 1e9;
+	const formattedSeconds = timeInSeconds.toFixed(2);
+	logger.info(
+		`Finished data retrieval for all dinosaurs, completed in ${formattedSeconds} seconds. Saving data to file now.`,
+	);
 	await writeData(data, "dinosaurData.json");
 }
 
