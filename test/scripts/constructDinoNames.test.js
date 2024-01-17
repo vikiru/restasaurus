1bd378a2809cd0fea8edb3764eed92718488d746
vikiru <72267229+vikiru@users.noreply.github.com>
2024-01-17 13:26:08 -0500

    Update test script to include coverage
    - additionally open test coverage html report after testing

--- test/scripts/constructDinoNames.test.js
+++ test/scripts/constructDinoNames.test.js
@@ -1,36 +1,48 @@
-const { assert, expect } = require('chai');
-const proxyquire = require('proxyquire');
+const { expect } = require('chai');
 const sinon = require('sinon');
+const sinonTest = require('sinon-test');
+const proxyquire = require('proxyquire');
 const logger = require('../../app/config/logger');
 const fetchData = require('../../app/utils/fetchData');
 const writeData = require('../../app/utils/writeData');
 const handleSetup = require('../../app/utils/handleSetup');
 
+const test = sinonTest(sinon);
+
 describe('constructDinoNames', function () {
-    it('should call the necessary functions and return the expected names', async function () {
-        const writeDataStub = sinon.stub(writeData, 'writeData').callsFake(() => {});
-        const fetchDataStub = sinon.stub(fetchData, 'fetchData').callsFake(() => {});
-        const retrieveNamesStub = sinon
-            .stub(handleSetup, 'retrieveAllDinoNames')
-            .callsFake(() => ['Tyrannosaurus', 'Stegosaurus']);
-        const handleStub = sinon.stub(handleSetup, 'handleUrls').callsFake(() => {});
-        const processStub = sinon.stub(handleSetup, 'processData').callsFake(() => ['Stegosaurus', 'Tyrannosaurus']);
+    it(
+        'should call the necessary functions and return the expected names',
+        test(async function () {
+            sinon.stub(logger.logger, 'info').callsFake(() => {});
+            sinon.stub(logger.logger, 'error').callsFake(() => {});
+            sinon.stub(logger.logger, 'http').callsFake(() => {});
 
-        const constructDinoNames = proxyquire('../../app/scripts/constructDinoNames', {
-            '../utils/writeData': writeDataStub,
-            '../utils/fetchData': fetchDataStub,
-            '../utils/handleSetup': retrieveNamesStub,
-            handleStub,
-            processStub,
-        });
+            const writeDataStub = sinon.stub(writeData, 'writeData').callsFake(() => {});
+            const fetchDataStub = sinon.stub(fetchData, 'fetchData').callsFake(() => {});
+            const retrieveNamesStub = sinon
+                .stub(handleSetup, 'retrieveAllDinoNames')
+                .callsFake(() => ['Tyrannosaurus', 'Stegosaurus']);
+            const handleStub = sinon.stub(handleSetup, 'handleUrls').callsFake(() => {});
+            const processStub = sinon
+                .stub(handleSetup, 'processData')
+                .callsFake(() => ['Stegosaurus', 'Tyrannosaurus']);
 
-        const { names } = await constructDinoNames.constructDinoNames();
+            const constructDinoNames = proxyquire('../../app/scripts/constructDinoNames', {
+                '../utils/writeData': writeDataStub,
+                '../utils/fetchData': fetchDataStub,
+                '../utils/handleSetup': retrieveNamesStub,
+                handleStub,
+                processStub,
+            });
 
-        expect(retrieveNamesStub.calledOnce).to.be.true;
-        expect(handleStub.calledOnce).to.be.true;
-        expect(processStub.calledOnce).to.be.true;
-        expect(names).to.have.length(2);
-        expect(names[0]).to.be.equal('Stegosaurus');
-        expect(names[1]).to.be.equal('Tyrannosaurus');
-    });
+            const { names } = await constructDinoNames.constructDinoNames();
+
+            expect(retrieveNamesStub.calledOnce).to.be.true;
+            expect(handleStub.calledOnce).to.be.true;
+            expect(processStub.calledOnce).to.be.true;
+            expect(names).to.have.length(2);
+            expect(names[0]).to.be.equal('Stegosaurus');
+            expect(names[1]).to.be.equal('Tyrannosaurus');
+        }),
+    );
 });
