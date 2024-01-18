6265d7a91cca920be49606cd94e745c5a2b2882d
vikiru <72267229+vikiru@users.noreply.github.com>
2024-01-17 22:56:38 -0500

    Fix writeData test

--- test/utils/writeData.test.js
+++ test/utils/writeData.test.js
@@ -1,28 +1,30 @@
-const { expect, done } = require('chai');
-const fs = require('fs');
-const { writeData } = require('../../app/utils/writeData');
+const { expect } = require('chai');
+const fs = require('fs').promises;
 const { MongooseData } = require('../../app/models/MongooseData');
 const path = require('path');
 
-describe('writeData', async function () {
+describe('writeData', function () {
     const dinoData = new MongooseData('Stegosaurus');
-    it('should save the data to the file when provided a valid object', async function () {
-        await writeData(dinoData, 'testDinoData.json');
-        const filePath = path.resolve(__dirname, '../../app/scripts/testDinoData.json');
-        fs.access(filePath, fs.constants.F_OK, (err) => {
-            expect(err).to.be.null;
-        });
+    const scriptsDir = path.resolve(__dirname, '../../app/scripts');
+    const filePath = path.join(scriptsDir, 'testDinoData.json');
 
-        fs.unlink(filePath, (err) => {
+    it('should save the data to the file when provided a valid object', async function () {
+        const { writeData } = require('../../app/utils/writeData');
+        await writeData(dinoData, 'testDinoData.json');
+        try {
+            await fs.access(filePath, fs.constants.F_OK);
+        } catch (err) {
             expect(err).to.be.null;
-        });
+        }
     });
 
     it('should throw an error when trying to save undefined object to file', async function () {
+        const { writeData } = require('../../app/utils/writeData');
         await writeData(undefined, 'testDinoData.json');
-        const filePath = path.resolve(__dirname, '../../app/scripts/testDinoData.json');
-        fs.access(filePath, fs.constants.F_OK, (err) => {
+        try {
+            await fs.access(filePath, fs.constants.F_OK);
+        } catch (err) {
             expect(err).to.not.be.null;
-        });
+        }
     });
 });
