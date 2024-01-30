2fd9328de38fce06eaf6327b81870598d305d06e
vikiru <72267229+vikiru@users.noreply.github.com>
2024-01-30 11:35:38 -0500

    Update tests

--- test/utils/writeData.test.js
+++ test/utils/writeData.test.js
@@ -1,7 +1,8 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const { expect } = require('chai');
+const { assert, expect } = require('chai');
+const sinon = require('sinon');
 
 const { MongooseData } = require('../../app/models/MongooseData');
 
@@ -10,17 +11,20 @@ describe('writeData', function () {
     const scriptsDir = path.resolve(__dirname, '../../app/scripts');
     const filePath = path.join(scriptsDir, 'testDinoData.json');
 
+    afterEach(function () {
+        sinon.restore();
+    });
+
     it('should save the data to the file when provided a valid object', async function () {
+        sinon.stub(fs, 'writeFile').resolves();
         const { writeData } = require('../../app/utils/writeData');
         await writeData(dinoData, 'testDinoData.json');
-        try {
-            await fs.access(filePath, fs.constants.F_OK);
-        } catch (err) {
-            expect(err).to.be.null;
-        }
+        assert.doesNotThrow(writeData, Error);
     });
 
     it('should throw an error when trying to save undefined object to file', async function () {
+        const error = new Error('Write file failed');
+        sinon.stub(fs, 'writeFile').rejects(error);
         const { writeData } = require('../../app/utils/writeData');
         await writeData(undefined, 'testDinoData.json');
         try {
